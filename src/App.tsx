9e9d0af630ab58cd5ee8f668e8eb05b6e1f0a217
Luiz Araújo - Control Informática <luiz.araujo@controlinformatica.com.br>
2020-02-04 16:12:58 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { CityList } from './city';
+import StoreProvider from './context';
+import Search from './search';
 
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StoreProvider>
+      <div className="App">
+        <header className="App-header">
+          <Search />
+          <CityList />
+        </header>
+      </div>
+    </StoreProvider>
   );
 }
 
---
+++ src/hook.tsx
@@ -0,0 +1,26 @@
+// import React from 'react';
+// import { useObserver } from 'mobx-react-lite';
+
+// export const useRootData = <Selection, ContextData, Store>(
+//   context: React.Context<ContextData>,
+//   storeSelector: (contextData: ContextData) => Store,
+//   dataSelector: (store: Store) => Selection
+// ) => {
+//   const value = React.useContext(context);
+//   if (!value) {
+//     throw new Error();
+//   }
+//   const store = storeSelector(value);
+//   return useObserver(() => {
+//     return dataSelector(store);
+//   });
+// };
+
+
+// import { storeContext } from "./context";
+// import { TStore } from "./store";
+
+// export const useRootData = <Selection>(
+//   dataSelector: (store: TStore) => Selection
+// ) =>
+//   useStoreData(storeContext, contextData => contextData!, dataSelector);
